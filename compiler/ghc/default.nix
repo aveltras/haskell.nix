ea58a94ab797b903186f284938ae44d5d76949df
aveltras <romain.viallard@outlook.fr>
2025-09-23 13:22:51 +0200

    bump ghc

--- compiler/ghc/default.nix
+++ compiler/ghc/default.nix
@@ -319,7 +319,7 @@ let
           then "ghc966"
         else if buildPackages.haskell.compiler ? ghc964
           then "ghc964"
-        else "ghc962";
+        else "ghc963";
     in
     buildPackages.haskell-nix.cabalProject' ({
       inherit compiler-nix-name;
